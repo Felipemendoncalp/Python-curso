f057d64ffbc94b35933cebcc48043bb6b6a0333a
Felipemendoncalp <felipemendoncalp@gmail.com>
2025-04-15 21:24:33 -0300

    Aula 51 desenpacotamento , Aula 52 tuplas

---
+++ Aula50.py
@@ -0,0 +1,5 @@
+lista= ['felipe', 'luiz' , 'adeilton' , 'natasha', 'rayla']
+lista.append('joão')
+indices = range(len(lista))
+for indice in indices :
+    print(indice , lista[indice])
---
+++ Aula51.py
@@ -0,0 +1,4 @@
+nomes = ['maria' , 'Helena' , 'Luiz']
+nomes= tuple(nomes)
+nomes = list(nomes)
+print(nomes)

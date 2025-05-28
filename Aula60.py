045022ade8b2e70c7085b5f0fb374cacc4f10a62
Felipemendoncalp <felipemendoncalp@gmail.com>
2025-05-28 19:19:47 -0300

    Operador Ternario

---
+++ Aula60.py
@@ -0,0 +1,8 @@
+# condicao= 10 == 11
+# variavel = 'valor' if condicao else 'Outro Valor'
+# print(variavel)
+digito = 9 
+novo_digito = digito if digito <= 9 else 0
+print(novo_digito)
+
+print('Valor' if True else 'Outro Valor' if True else 'fim')
